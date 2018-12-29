ac5004cf0558a36df9edbebf50a9f99f1bded558
JayRodrig <mrjosemlrod@gmail.com>
2018-12-29 17:40:23 -0500

    deleted services dir, not needed

--- services/math.js
+++
@@ -1,7 +0,0 @@
-const math = {};
-math.add = (nOne, nTwo) => nOne + nTwo;
-math.subtract = (nOne, nTwo) => nOne - nTwo;
-math.divide = (nOne, nTwo) => nOne / nTwo;
-math.multiply = (nOne, nTwo) => nOne * nTwo;
-
-module.exports = math;
